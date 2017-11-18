3aeba00b0ade124edf390c7215196753822cbfd3
laboratorio <laboratorio@LAB9-12>
2017-11-18 16:37:12 -0600

    Se agrega controlador de Productos

---
+++ Ulatina.Topicos.Database.Example/Ulatina.Topicos.Database.Example/AdventureWorks2014EntitiesContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+public class AdventureWorks2014EntitiesContext : DbContext
+{
+    // You can add custom code to this file. Changes will not be overwritten.
+    // 
+    // If you want Entity Framework to drop and regenerate your database
+    // automatically whenever you change your model schema, please use data migrations.
+    // For more information refer to the documentation:
+    // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+    public AdventureWorks2014EntitiesContext() : base("name=AdventureWorks2014EntitiesContext")
+    {
+    }
+
+    public System.Data.Entity.DbSet<AdventureWorks.Products.Model.Product> Products { get; set; }
+
+    public System.Data.Entity.DbSet<AdventureWorks.Products.Model.ProductModel> ProductModels { get; set; }
+
+    public System.Data.Entity.DbSet<AdventureWorks.Products.Model.ProductSubcategory> ProductSubcategories { get; set; }
+}
